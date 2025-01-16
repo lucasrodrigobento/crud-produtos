3cbebfb3332a3fb493c197786a8da75c26f1539d
Lucas Bento <lucasrodrigobento@outlook.com>
2025-01-16 14:46:26 -0300

    fix: rotas de login defult do laravel removida ee redirecionamento feito para admin.

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,17 +1,17 @@
 <nav class="-mx-3 flex flex-1 justify-end">
     @auth
         <a
-            href="{{ url('/dashboard') }}"
+            href="{{ route('filament.admin.pages.dashboard') }}"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
         >
-            Dashboard
+            Painel de Controle
         </a>
     @else
         <a
             href="{{ route('login') }}"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
         >
-            Log in
+            Entrar
         </a>
 
         @if (Route::has('register'))
@@ -19,7 +19,7 @@
                 href="{{ route('register') }}"
                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
             >
-                Register
+                Cadastrar
             </a>
         @endif
     @endauth
